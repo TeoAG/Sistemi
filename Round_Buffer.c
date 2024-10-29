5a2a7e7d5d05eed31ce2841d88d316d428084681
Call_Me_Teoo <130556211+TeoAG@users.noreply.github.com>
2024-10-29 07:28:37 +0100

    Round_Buffer.c

---
+++ Round_Buffer.c
@@ -0,0 +1,28 @@
+#include <stdint.h>
+
+#define BUFFER_SIZE 1024
+
+typedef struct {
+    uint8_t data[BUFFER_SIZE];
+    uint16_t head;
+    uint16_t tail;
+} round_buffer_t;
+
+void put(round_buffer_t* buffer, uint8_t* data) {
+    if ((buffer->head + 1) % BUFFER_SIZE == buffer->tail) {
+        // Buffer is full, discard or delay data
+    } else {
+        buffer->data[buffer->head] = *data;
+        buffer->head = (buffer->head + 1) % BUFFER_SIZE;
+    }
+}
+
+uint8_t get(round_buffer_t* buffer) {
+    if (buffer->head == buffer->tail) {
+        // Buffer is empty, wait or retrieve data
+    } else {
+        uint8_t data = buffer->data[buffer->tail];
+        buffer->tail = (buffer->tail + 1) % BUFFER_SIZE;
+        return data;
+    }
+}
